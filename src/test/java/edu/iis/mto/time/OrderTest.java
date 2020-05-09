06cf2e98b606726cb27a034a25bc4367afabce4f
Patryk Janowski <215745@edu.p.lodz.pl>
2020-05-09 19:34:09 +0200

    testIfOrderStateIsCreated added

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,5 +1,8 @@
 package edu.iis.mto.time;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,12 +14,19 @@ public class OrderTest {
     @Before
     public void setUp() {
         this.order = new Order();
-        this.order.addItem(new OrderItem());
-        this.order.submit();
     }
 
     @Test(expected = OrderExpiredException.class)
     public void testIfOrderExpiredExceptionThrown() {
+        this.order.addItem(new OrderItem());
+        this.order.submit();
         this.order.confirm(DateTime.now().plusHours(30));
     }
+
+    @Test
+    public void testIfOrderStateIsCreated() {
+        assertThat(Order.State.CREATED, is(this.order.getOrderState()));
+    }
+
+
 }
